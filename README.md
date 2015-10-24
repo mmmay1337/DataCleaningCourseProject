688b98ec23aadb60742fa5c5325a6902fef6b088
yimp <yiimay.phang@gmail.com>
2015-10-24 17:31:24 +1100

    submitting Data Cleansing project with 3 files

---
+++ README.md
@@ -0,0 +1,31 @@
+##  READ ME ##
+#   this READ ME file will describe the relevant files required for preparing the tidy dataset,
+#   along with what is required to run the run_analysis.R file.
+
+##  RUNNING THE SCRIPT ##
+#   The R script has been set up in an automated manner - it will automatically detect if the
+#   current directory contains the relevant files to be read in (otherwise it will download it).
+#   This is achieved by using the file.exists() function.
+#   All that is required is to run the script to generate the outputs.
+
+##  RELEVANT FILES ##
+#   the following files come from the zip file, in the UCI HAR Dataset folder. 
+
+#   features identifiers #
+#   activity_labels.txt: this is the corresponding description for each activity ID
+#   features.txt: this is the descriptive names of the features captured in the X_<test/train> dataset
+
+##  relevant features / activity ##
+#   <test/train>/Subect_<test/train>: this is the corresponding subject ID observation
+#                                     for each row, in the <test/train> dataset
+#   <test/train>/X_<test/train>:  this file contains the relevant features, from 
+#                                 the <test/train> dataset
+#   <test/train>/y_<test/train>:  this is the activity ID for the task being performed, 
+#                                 from the <test/train> dataset
+
+##  EXPECTED OUTPUTS ##
+# note that any intermediate datasteps are cleared throughout the code, so that the only 
+# remaining R objects are the CombinedData and Summary data.
+
+## for more information on the general procedures of preparing the dataset (and variables),
+## please refer to the codebook.
---
+++ run_analysis.R
@@ -0,0 +1,73 @@
+# read in necessary libraries
+library(dplyr)
+library(reshape)
+
+############## Step 1: Merges the training and the test sets to create one data set ##############
+
+## download zip if it doesnt exist ##
+if(!file.exists("getdata-projectfiles-UCI HAR Dataset.zip")|file.exists("UCI HAR Dataset")){
+  ZipURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(ZipURL, destfile="./getdata-projectfiles-UCI HAR Dataset.zip")
+  ZipFile <- unzip("getdata-projectfiles-UCI HAR Dataset.zip", exdir = getwd())
+}
+
+
+## create tables from the unzipped files ##
+dir_name <- "./UCI HAR Dataset/"
+features_data <- read.table(paste(dir_name, "features.txt", sep="")) # read in features name, these are variables for each test and training datasets
+features <- features_data$V2 #these are the column names for each dataset
+rm("features_data") # clear unnecessary data from the workspace for memory management
+
+## read in training data ##
+sub_train <- read.table(paste(dir_name, "train/subject_train.txt", sep=""), col.names = "SubjectID") #corresponding subject ID
+X_train <- read.table(paste(dir_name, "train/X_train.txt", sep=""), col.names = features) # training features
+Y_train <- read.table(paste(dir_name, "train/Y_train.txt", sep=""), col.names = "activityID") # these are the activity labels (as ID)
+train  <- cbind(X_train, sub_train, Y_train) # combine the features and labels into 1 training dataset
+
+## read in test data ##
+sub_test <- read.table(paste(dir_name, "test/subject_test.txt", sep=""), col.names = "SubjectID") #corresponding subject ID
+X_test <- read.table(paste(dir_name, "test/X_test.txt", sep=""), col.names = features) # testing features
+Y_test <- read.table(paste(dir_name, "test/Y_test.txt", sep=""), col.names = "activityID") # these are the activity labels (as ID)
+test  <- cbind(X_test, sub_test, Y_test) # combine the features and labels into 1 testing dataset
+
+
+## combine into one dataset ##
+CombinedData  <- rbind(train, test)
+
+# clear unnecessary data from the workspace for memory management (retain the unzipped files just in case required)
+rm("X_train", "Y_train", "X_test", "Y_test", "train", "test")
+
+############## Step 2: Extract only the measurements on the mean and std for each measurement ##############
+
+# find out which columns contain the names mean and std, then create a vector on the columns to retain
+Col_Mean <- names(select(CombinedData, contains("mean", ignore.case = FALSE))) # dont want to get the angles
+Col_Std <- names(select(CombinedData, contains("std", ignore.case = "FALSE"))) # dont want to get the angles
+Col_Retain <- c("SubjectID", "activityID", Col_Mean, Col_Std)# create a vector of the columns to retain
+
+CombinedData <- CombinedData[, Col_Retain] # retain only the relevant columns
+
+############## Step 3: use descriptive activity names to name the activities in the data set ##############
+# read in the descriptive activity names data
+Desc_Activity <- read.table("./CleaningandGettingData/CourseProject/UCI HAR Dataset/activity_labels.txt", col.names = c("activityID", "ActivityDesc"))
+# merge the activity descriptions by activity ID, dont need the merge by vars, this automatically puts the merge key at the front of the data
+CombinedData <- merge(CombinedData, Desc_Activity) # this will merge by activityID
+CombinedData <- select(CombinedData, -(activityID)) # drop the activityID since data now contains desc
+############## Step 4: appropriately label the dataset with descriptive variable names ##############
+# get rid of the dots first - apply the rename on the final vector
+names(CombinedData) <- gsub("[.]", "", names(CombinedData))
+# get rid of the repeated words for Body
+names(CombinedData) <- gsub("BodyBody", "Body", names(CombinedData))
+
+#remove unnecessary R objects
+rm("Desc_Activity", "Col_Mean", "Col_Std")
+
+# use the codebook, will also have to create a codebook
+
+
+############## Step 5: From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# should look like this
+
+# melt the data
+Summary <- melt(CombinedData, id = c("SubjectID", "ActivityDesc"))
+Summary <- cast(Summary, SubjectID + ActivityDesc ~ variable, mean) #obtain the average for each item
+write.table(Summary, file = "./Summary.txt", row.names = FALSE)
